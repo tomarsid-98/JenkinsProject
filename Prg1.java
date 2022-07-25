a4262a0844f2d920c00cd8ba84cd0dad499279fe
unknown <amdoconb034@iiht.tech>
2022-07-25 12:39:00 +0530

    First Program

---
+++ Prg1.java
@@ -0,0 +1,9 @@
+public class Prg1
+{
+	public static void main(String [] args)
+	{
+		int num1=Integer.parseInt(args[0]);
+		int num2=Integer.parseInt(args[0]);
+		System.out.println("Addition of "+num1+" and "+num2+" is "+(num1+num2));
+	}
+}
