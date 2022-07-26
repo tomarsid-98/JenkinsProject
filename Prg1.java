83419bb7155c1f8aa0b680f40b971f85d09384b5
unknown <amdoconb034@iiht.tech>
2022-07-26 12:39:09 +0530

    Added Multiplication, Division and Subtraction

--- Prg1.java
+++ Prg1.java
@@ -7,6 +7,6 @@ public class Prg1
 		System.out.println("Addition of "+num1+" and "+num2+" is "+(num1+num2));
 		System.out.println("Subtraction of "+num1+" and "+num2+" is "+(num1-num2));
 		System.out.println("Multiplication of "+num1+" and "+num2+" is "+(num1*num2));
-		System.out.println((num2 == 0)?"Division is Not Possible as Denominator is Zero":"Division of "+num1+" and "+num2+" is "+(double)(num1/num2));
+		System.out.println((num2 == 0)?"Division is Not Possible as Denominator is Zero":"Division of "+num1+" and "+num2+" is "+(double)(num1/num2*1.0));
 	}
 }
